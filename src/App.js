366fde47a5d08c8b72d2517b41b781a2624bd5ce
Vadim Balabin <vadikflint@gmail.com>
2021-02-14 15:03:06 +0400

    refactor: remove optionals a path

--- src/App.js
+++ src/App.js
@@ -3,50 +3,36 @@ import { PageContainer, Competition, Team, CardList } from './components';
 import { Alert } from 'antd'
 const { ErrorBoundary } = Alert;
 
-// TODO: Fix route path
-const App = () => {
-
-  return (
-    <PageContainer>
-      <ErrorBoundary>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={CardList}
-          />
-          <Route
-            exact
-            path="/competition/:id/:tab"
-            component={Competition}
-          />
-          <Route
-            exact
-            path="/competition/:id"
-            component={Competition}
-          />
-          <Route
-            exact
-            path="/teams"
-            component={CardList}
-          />
-          <Route
-            exact
-            path="/team/:id/:tab"
-            component={Team}
-          />
-          <Route
-            exact
-            path="/team/:id"
-            component={Team}
-          />
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </ErrorBoundary>
-    </PageContainer>
-  )
-};
+const App = () => (
+  <PageContainer>
+    <ErrorBoundary>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={CardList}
+        />
+        <Route
+          exact
+          path="/competition/:id/:tab?"
+          component={Competition}
+        />
+        <Route
+          exact
+          path="/teams"
+          component={CardList}
+        />
+        <Route
+          exact
+          path="/team/:id/:tab?"
+          component={Team}
+        />
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </ErrorBoundary>
+  </PageContainer>
+)
 
 export default App;
